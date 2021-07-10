6d7a9972c3caff06260575fa60c150df7527a6ef
noushad <devmanzur@gmail.com>
2021-07-10 19:34:18 +0600

    added trello api client credentials to url

--- TrelloApiClientBackend/Clients/TrelloRestapiClient.cs
+++ TrelloApiClientBackend/Clients/TrelloRestapiClient.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using TrelloApiClientBackend.Brokers.Trello;
 
@@ -10,14 +11,21 @@ namespace TrelloApiClientBackend.Clients
     {
         private readonly HttpClient _httpClient;
 
-        public TrelloRestapiClient(HttpClient httpClient)
+        public TrelloRestapiClient(HttpClient httpClient,IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _apiKey = configuration.GetValue<string>("Trello:ApiKey");
+            _userAuthToken = configuration.GetValue<string>("Trello:UserAuthToken");
         }
+        
+        private readonly string _apiKey;
+        private readonly string _userAuthToken;
+        private string SetupUrlAuthorization(string path) => $"{path}?key={_apiKey}&token={_userAuthToken}";
+
 
         public async Task<Result<TResponse>> Get<TResponse>(string path)
         {
-            var response = await _httpClient.GetAsync(path);
+            var response = await _httpClient.GetAsync(SetupUrlAuthorization(path));
             var responseBody = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
